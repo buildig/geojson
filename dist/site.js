b6511ab932306bc469d4c39ae5627d723eb8ef7e
Tormi Tabor <tormi.tabor@gmail.com>
2016-12-06 16:09:43 +0200

    Use maakaart's satellite layer (#1)

--- dist/site.js
+++ dist/site.js
@@ -27027,12 +27027,6 @@ module.exports = function(context) {
 
     return function(selection) {
 
-      var EPSG_3301 = new L.Proj.CRS('EPSG:3301', '+proj=lcc +lat_1=59.33333333333334 +lat_2=58 +lat_0=57.51755393055556 +lon_0=24 +x_0=500000 +y_0=6375000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs', {
-          resolutions: [4000, 2000, 1000, 500, 250, 125, 62.5, 31.25, 15.625, 7.8125, 3.90625, 1.953125, 0.9765625, 0.48828125, 0.244140625, 0.122070313, 0.061035156, 0.030517578, 0.015258789],
-          origin: [40500, 5993000],
-          bounds: L.bounds([40500, 5993000], [1064500, 7017000])
-      });
-
         var layers;
 
         if (!(/a\.tiles\.mapbox.com/).test(L.mapbox.config.HTTP_URL)) {
@@ -27056,10 +27050,8 @@ module.exports = function(context) {
                 layer: L.mapbox.tileLayer('mapbox.satellite')
             }, {
                 title: 'FOTO',
-                layer: L.tileLayer('http://tiles.maaamet.ee/tm/s/1.0.0/foto/{z}/{x}/{y}.png', {
-                   attribution: 'Maps &copy; <a title="Maa-amet" href="http://geoportaal.maaamet.ee" target="_blank">Maa-amet</a>',
-                   crs: EPSG_3301,
-                   tms: true
+                layer: L.tileLayer('http://kaart.maakaart.ee/orto/{z}/{x}/{y}.jpeg', {
+                attribution: 'FOTO &copy; <a title="Maa-amet" href="http://geoportaal.maaamet.ee" target="_blank">Maa-amet</a>'
                 })
             }, {
                 title: 'OSM',
